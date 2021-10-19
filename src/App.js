b6a9fa1453b7a45cc1dbfe2dbfa69e60996dbda3
Juliana Ifionu <julieifionu@gmail.com>
2021-10-19 18:18:12 +0100

    Create counter to add and remove item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Counters from "./components/counters";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div className="container">
+      <Counters />
     </div>
   );
 }
